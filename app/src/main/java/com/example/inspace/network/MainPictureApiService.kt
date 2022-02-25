89302263038c6b2fe4cc35078656df1a36487cc1
Marmalaade <79687328+Marmalaade@users.noreply.github.com>
2022-02-25 21:33:51 +0300

    refactor: retrofitObject initialization

--- app/src/main/java/com/example/inspace/network/MainPictureApiService.kt
+++ app/src/main/java/com/example/inspace/network/MainPictureApiService.kt
@@ -1,22 +1,10 @@
 package com.example.inspace.network
 
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.example.inspace.retrofitobject.RetrofitObjectInitialization
 import retrofit2.Call
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://api.nasa.gov/planetary/"
-
-private val moshi = Moshi.Builder()
-    .add(KotlinJsonAdapterFactory())
-    .build()
-
-private val retrofit = Retrofit.Builder()
-    .addConverterFactory(MoshiConverterFactory.create(moshi))
-    .baseUrl(BASE_URL)
-    .build()
 
 interface MainPictureApiService {
     @GET("apod?api_key=g6LnNorzWiIzqiTSmEbEell4AHdfz7w6dDfuOzvb")
@@ -25,6 +13,6 @@ interface MainPictureApiService {
 
 object MainPictureApi {
     val retrofitService: MainPictureApiService by lazy {
-        retrofit.create(MainPictureApiService::class.java)
+        RetrofitObjectInitialization.initRetrofit(BASE_URL).create(MainPictureApiService::class.java)
     }
 }
---
+++ app/src/main/java/com/example/inspace/retrofitobject/RetrofitObjectInitialization.kt
@@ -0,0 +1,21 @@
+package com.example.inspace.retrofitobject
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+object RetrofitObjectInitialization {
+
+    fun initRetrofit(url: String): Retrofit {
+        val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+        return Retrofit.Builder()
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .baseUrl(url)
+            .build()
+    }
+
+}

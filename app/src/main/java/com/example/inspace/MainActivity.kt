4694a582c4d2a51592294d1c1315ac563c75a060
Marmalaade <79687328+Marmalaade@users.noreply.github.com>
2022-03-25 20:26:05 +0300

    feat: add workManager constraints

--- app/src/main/java/com/example/inspace/MainActivity.kt
+++ app/src/main/java/com/example/inspace/MainActivity.kt
@@ -1,23 +1,17 @@
 package com.example.inspace
 
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.work.ExistingPeriodicWorkPolicy
-import androidx.work.PeriodicWorkRequestBuilder
-import androidx.work.WorkManager
-import com.bumptech.glide.Glide
+import androidx.work.*
 import com.example.inspace.work.RefreshDataWorker
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import java.util.concurrent.TimeUnit
 
@@ -43,7 +37,21 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupRecurringWork() {
-        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(1, TimeUnit.DAYS).build()
+
+        val workerConstraints = Constraints.Builder()
+            .apply {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    setRequiresDeviceIdle(true)
+                }
+            }
+            .setRequiresBatteryNotLow(true)
+            .setRequiresStorageNotLow(true)
+            .setRequiredNetworkType(NetworkType.UNMETERED)
+            .build()
+
+        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(1, TimeUnit.DAYS)
+            .setConstraints(workerConstraints)
+            .build()
         WorkManager.getInstance(applicationContext).enqueueUniquePeriodicWork(
             RefreshDataWorker.WORK_NAME,
             ExistingPeriodicWorkPolicy.KEEP,
